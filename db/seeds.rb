a327a83f81853370cdc66aa67d34eb2ff559b904
github username <github email address>
2019-03-04 16:33:30 -0500

    separate front and back, add user seeds, cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Jon = User.create(name: "Jon Freed", location: "Brooklyn")
+Rosanna = User.create(name: "Rosanna Brown", location: "Manhattan")
+Stephanie = User.create(name: "Stephanie Abud", location: "Queens")
+Joseph = User.create(name: "Joseph Gao", location: "Brooklyn")
+Kudzanayi = User.create(name: "Kudzanayi Dzvairo", location: "Manhattan")
+Simon = User.create(name: "Simon Kong", location: "Queens")
+Mo = User.create(name: "Mohammad Ali", location: "Bronx")
+Helen = User.create(name: "Helen Liutongco", location: "Bronx")
+Prince = User.create(name: "Prince Wilson", location: "Manhattan")
+Cassidy = User.create(name: "Cassidy Samelian", location: "Brooklyn")
+Jesus = User.create(name: "Jesus Gonzalez", location: "New Jersey")
+Shivani = User.create(name: "Shivani Brijmohan", location: "New Jersey")
+Edwin = User.create(name: "Edwin Lin", location: "Brooklyn")
+Raymond = User.create(name: "Raymond Chow", location: "Queens")
+Norbert = User.create(name: "Norbert Lam", location: "Queens")
+Jun = User.create(name: "Jun Lee", location: "Manhattan")
+Wei = User.create(name: "Wei Huang", location: "Manhattan")
+Lizzy = User.create(name: "Lizzy Le", location: "Brooklyn")
+
+
